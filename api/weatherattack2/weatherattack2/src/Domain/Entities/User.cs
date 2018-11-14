327432de86cc8f2a4c42669ef57c38e313ab5896
matheus teixeira câmara <matheus.t98@gmail.com>
2018-11-14 14:19:50 -0200

    first commit

---
+++ api/weatherattack2/weatherattack2/src/Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace weatherattack2.src.Domain.Entities
+{
+    public class User
+    {
+        public long Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string Email { get; private set; }
+
+    }
+}
