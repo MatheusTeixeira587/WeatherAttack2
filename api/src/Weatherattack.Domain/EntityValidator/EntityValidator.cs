a85153773ecdbf04783920962342e57e7cb2e176
matheus teixeira câmara <matheus.t98@gmail.com>
2018-11-27 15:14:46 -0200

    feat(valit)

--- api/src/Weatherattack.Domain/EntityValidator/EntityValidator.cs
+++ api/src/Weatherattack.Domain/EntityValidator/EntityValidator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Valit;
 using weatherattack2.src.Domain.Entities;
 using weatherattack2.src.Domain.EntityValidator.Rules.User;
 using weatherattack2.src.Domain.Notifications.User;
@@ -21,6 +22,17 @@ namespace weatherattack2.src.Domain.Validator
             if (user.Username == null || user.Username.Trim().Length < UserRules.UsernameRules.MinLength || user.Username.Trim().Length > UserRules.UsernameRules.MaxLength)
                 user.AddNotification(UserNotifications.InvalidUsername);
 
+        }
+
+        public static void a()
+        {
+            var result = ValitRules<User>
+                .Create()
+                .Ensure(u => u.Email, _ => _
+                    .Required()
+                    .Email()
+                    .MaxLength(UserRules.EmailRules.MaxLength));
+                    
         }
     }
 }
